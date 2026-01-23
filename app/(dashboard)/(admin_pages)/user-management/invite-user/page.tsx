8da69b94ef221d00a43551c43177734baee78245
Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-01-23 20:58:50 +0600

    invitation link showing under the invite form

--- app/(dashboard)/(admin_pages)/user-management/invite-user/page.tsx
+++ app/(dashboard)/(admin_pages)/user-management/invite-user/page.tsx
@@ -3,6 +3,7 @@
 import Button from "@/components/libs/Button";
 import Card from "@/components/libs/Card";
 import DropdownMenus from "@/components/libs/DropdownMenus";
+import IconButton from "@/components/libs/IconButton";
 import OutlinedInput from "@/components/libs/OutlinedInput";
 import Typography from "@/components/libs/Typography";
 import { userRoleOptions } from "@/components/user/UpdateUserRoleModal";
@@ -10,7 +11,11 @@ import { IInviteSchema, inviteSchema } from "@/schema/auth.schema";
 import { isFetchBaseQueryError } from "@/store/baseQuery";
 import { useSendInvitationMutation } from "@/store/features/auth";
 import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { BiCopy } from "react-icons/bi";
+import { FaCheck } from "react-icons/fa";
 import { toast } from "react-toastify";
 
 export enum USER_ROLE {
@@ -21,6 +26,8 @@ export enum USER_ROLE {
 
 const Page = () => {
   const [inviteUser, { isLoading, error }] = useSendInvitationMutation();
+  const [copied, setCopied] = useState(false);
+  const [inviteLink, setInviteLink] = useState("");
   const {
     register,
     control,
@@ -36,11 +43,25 @@ const Page = () => {
 
   async function onSubmit(data: IInviteSchema) {
     try {
-      await inviteUser(data).unwrap();
+      const res = await inviteUser(data).unwrap();
+      setInviteLink(res.invitationLink);
       toast.success("Invitation sent successfully");
     } catch (error: any) {
       toast.error(error.message);
     }
+  }
+
+  function handleCopy() {
+    let timer: NodeJS.Timeout | null = null;
+    navigator.clipboard.writeText(inviteLink);
+    setCopied(true);
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+      setCopied(false);
+    }, 2000);
   }
 
   return (
@@ -68,9 +89,26 @@ const Page = () => {
           )}
         />
 
-        <Typography color="error">
-          {isFetchBaseQueryError(error) && error.data.message}
-        </Typography>
+        {error && (
+          <Typography color="error">
+            {isFetchBaseQueryError(error) && error.data.message}
+          </Typography>
+        )}
+
+        {inviteLink && (
+          <div>
+            <Typography>Invitation link:</Typography>
+            <div className="flex items-center">
+              <input value={inviteLink} readOnly className="rounded-r-none!" />
+              <IconButton
+                onClick={handleCopy}
+                className="rounded-md rounded-l-none"
+              >
+                {!copied ? <BiCopy /> : <FaCheck />}
+              </IconButton>
+            </div>
+          </div>
+        )}
         <Button
           loading={isLoading}
           className="ml-auto"
